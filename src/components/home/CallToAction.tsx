39ff9afddcce188f79f889650158825a6fd606c2
rocketbro <asherpope@gmail.com>
2025-05-21 14:24:58 -0400

    update links and metadata

--- src/components/home/CallToAction.tsx
+++ src/components/home/CallToAction.tsx
@@ -22,23 +22,20 @@ const CallToAction = ({ id = 'cta' }: CallToActionProps) => {
           <div
             className="flex flex-row flex-wrap gap-6 items-center justify-center"
           >
-            <Link
-              href="https://x.com/intrinsic_labs"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="button-primary text-center flex flex-row gap-2 items-center">
+            <a
+              href="tel:+18036278166"
+              className="button-primary text-center flex flex-row gap-2 items-center"
+            >
               <FiPhone size={20} />
               Give us a call
-            </Link>
+            </a>
 
-            <Link
-              href="https://x.com/intrinsic_labs"
-              target="_blank"
-              rel="noopener noreferrer"
+            <a
+              href="mailto:helloworld@intrinsiclabs.co"
               className="button-primary text-center flex flex-row gap-2 items-center">
               <FiMail size={20} />
               Email us
-            </Link>
+            </a>
 
           </div>
         </div>
